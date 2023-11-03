40993f80a3178b36204184b17e8cda6f9679235b
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-11-03 12:58:30 +0530

    Initial setup done

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {PORT} = require('./config/serverConfig');
+
+const setupAndStartServer = () => {
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.listen(PORT, () => {
+        console.log(`Server started at Port ${PORT}`);
+    });
+}
+
+setupAndStartServer();
