5624f09fba038a8604d597026871ce6d69140832
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-11-13 13:04:40 +0530

    Added rabbit mq config

--- src/index.js
+++ src/index.js
@@ -4,17 +4,19 @@ const {PORT} = require('./config/serverConfig');
 //const {sendBasicEmail} = require('./services/email-service');
 const jobs = require('./utils/job');
 const TicketController = require('./controllers/ticket-controller');
+const {createChannel} = require('./utils/messageQueue');
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.urlencoded({extended: true}));   
 
+   // const channel = await createChannel(); 
     app.post('/api/v1/tickets', TicketController.create);
 
     app.listen(PORT, () => {
         console.log(`Server started at Port ${PORT}`);
-        jobs();
+        //jobs();
         // sendBasicEmail(
         //     'flightservice601@gmail.com',
         //     'ayushsunilraj@gmail.com',
